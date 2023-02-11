fa83573c9aadcbc4b330997e6551a4e51281321c
宋子豪 <suka233@outlook.com>
2023-02-11 10:49:19 +0800

    feat: 新增emoji组件

---
+++ src/components/emojiIcon/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <span v-if="props.iconUnicode">
+    <span v-html="`&#x${props.iconUnicode};`"></span>
+  </span>
+
+</template>
+
+<script setup lang="tsx">
+const props = defineProps<{
+  iconUnicode?: string;
+}>()
+</script>
+
+<script lang="tsx">
+/**
+ * @description emoji图标
+ * thanx: @fatevase
+ * https://github.com/fatevase/SiYuanDailyViews/blob/dev/src/components/Emoji.vue
+ */
+export default {
+  name:'EmojiIcon',
+}
+</script>
+
+<style scoped>
+
+</style>

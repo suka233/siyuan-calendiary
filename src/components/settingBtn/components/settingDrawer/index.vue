ffb4da753f99cc4d7acae2eb83bcc8df64fbb36f
宋子豪 <suka233@outlook.com>
2023-02-13 17:21:35 +0800

    refactor: (/@/../SettingDrawer) 提取 设置drawer 为组件

--- src/components/settingBtn/components/settingDrawer/index.vue
+++ src/components/settingBtn/components/settingDrawer/index.vue
@@ -1,9 +1,32 @@
-<template></template>
+<template>
+    <a-drawer v-model:visible="visible"></a-drawer>
+</template>
 
 <script lang="ts">
 export default {
     name: 'SettingDrawer',
 };
+</script>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    visible: boolean;
+}>();
+
+const emit = defineEmits<{
+    (event: 'update:visible', visible: boolean): void;
+}>();
+
+const visible = computed({
+    get() {
+        return props.visible;
+    },
+    set(visible) {
+        emit('update:visible', visible);
+    },
+});
 </script>
 
 <style scoped></style>
